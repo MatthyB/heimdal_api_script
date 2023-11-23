2c9f1f467fa395d4ddcd37d26ca266b1c256d8dc
MatthyB <120468575+MatthyB@users.noreply.github.com>
2023-11-23 19:29:33 +0100

    Update heimdal_script.py

--- heimdal_script.py
+++ heimdal_script.py
@@ -96,8 +96,9 @@ def main(config_path):
                         })
 
                 for customer_info in customer_data:
-                    customer_id = customer_info["customer_id"]
-                    customer_name = customer_info["customer_name"]
+                    if isinstance(event, dict):
+                        customer_id = customer_info["customer_id"]
+                        customer_name = customer_info["customer_name"]
 
                     for heimdal_module in heimdal_modules_15minutes:
                         # Calculate the start and end dates
@@ -151,8 +152,9 @@ def main(config_path):
                         })
 
                 for customer_info in customer_data:
-                    customer_id = customer_info["customer_id"]
-                    customer_name = customer_info["customer_name"]
+                    if isinstance(event, dict):
+                        customer_id = customer_info["customer_id"]
+                        customer_name = customer_info["customer_name"]
 
                     for heimdal_module in heimdal_modules_1hours:
                         # Calculate the start and end dates
